3a15ac73e88a3515ccabbd20f44a5c8f8d558897
Eduardo Santos <3dmena@gmail.com>
2025-02-04 02:11:12 -0600

    feat: base sensor model up

---
+++ src/sensors/base.py
@@ -0,0 +1,30 @@
+"""Base model for sensor reading"""
+
+class SensorBase:
+    """Base class for all sensors"""
+    def __init__(self, name: str, pin:int = None):
+        """Builder"""
+        self.name = name
+        self.pin = pin
+        self.connected = False
+        self.last_reading = None
+    
+    def initialize(self):
+        """Initialize the sensor hardware -- override in child classes"""
+        raise NotImplementedError
+
+    def read(self):
+        """Read the data from he sensor -- override in child classes"""
+        raise NotImplementedError
+    
+    def get_reading(self):
+        """Get sensor reading with error handling"""
+
+        try:
+            if not self.connected:
+                 return None
+            self.last_reading = self.read()
+        except Exception as e:
+            print(f"Error reading {self.name}: {str(e)}")
+            return None
+
