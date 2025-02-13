bb0e30c26a684d9e2bcdc35c80a74ce9255e9f9d
Eduardo Santos <3dmena@gmail.com>
2025-02-12 21:23:59 -0600

    feat: updated i2d protocols for the last version of the hardware esp32 (green board)

--- src/protocols/i2c.py
+++ src/protocols/i2c.py
@@ -1,8 +1,18 @@
 """I2C protocol implementation"""
+from machine import I2C, Pin 
+
 class I2CDevice:
+    """
+    A class to represent an I2C device.
+    Args:
+        bus_num (int): The I2C bus number.
+        address (int): The I2C address of the device.
+    Attributes:
+        bus (I2C): The I2C bus object.
+        address (int): The I2C address of the device.
+    """
     def __init__(self, bus_num, address):
-        from machine import I2C, Pin
-        self.bus = I2C(bus_num, scl=Pin(22), sda=Pin(21))
+        self.bus = I2C(bus_num, scl=Pin(21), sda=Pin(23))
         self.address = address
 
     def read_bytes(self, register, length):
