4647fc5c2b5e99db92c22253bad226197438f67b
Eduardo Santos <3dmena@gmail.com>
2025-03-11 21:56:10 -0600

    feat: updated i2c to softi2c

--- src/protocols/i2c.py
+++ src/protocols/i2c.py
@@ -1,18 +1,18 @@
 """Implementación del protocolo I2C actualizado"""
-from machine import I2C, Pin
+from machine import Pin
+from machine import SoftI2C as I2C
 
 class I2CDevice:
     """
     Clase que representa un dispositivo I2C.
     
     Args:
-        bus_num (int): Número del bus I2C.
         address (int): Dirección I2C del dispositivo.
         scl_pin (int): Número de pin para SCL.
         sda_pin (int): Número de pin para SDA.
     """
-    def __init__(self, bus_num, address, scl_pin=21, sda_pin=23):
-        self.bus = I2C(bus_num, scl=Pin(scl_pin), sda=Pin(sda_pin))
+    def __init__(self, address, scl_pin=21, sda_pin=23):
+        self.bus = I2C( scl=Pin(scl_pin), sda=Pin(sda_pin))
         self.address = address
 
     def read_bytes(self, register, length):
@@ -23,11 +23,11 @@ class I2CDevice:
         """Escribe un arreglo de bytes en el registro especificado."""
         self.bus.writeto_mem(self.address, register, data)
 
-def init_i2c(bus_num=1, scl_pin=21, sda_pin=23):
+def init_i2c(scl_pin=21, sda_pin=23):
     """
     Función de conveniencia para inicializar el bus I2C con los pines especificados.
     
     Returns:
         I2C: Objeto I2C configurado.
     """
-    return I2C(bus_num, scl=Pin(scl_pin), sda=Pin(sda_pin))
+    return I2C(scl=Pin(scl_pin), sda=Pin(sda_pin))
