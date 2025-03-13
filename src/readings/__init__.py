0f5f24557e3cb3d50311e0a648cf51306f63b85a
Eduardo Santos <3dmena@gmail.com>
2025-03-12 18:48:50 -0600

    draft: init for readings downgraded for simplicity

--- src/readings/__init__.py
+++
@@ -1,8 +0,0 @@
-from readings.sensor_reader import SensorReader
-
-# Crear una instancia global
-reader = SensorReader()
-
-# Función auxiliar
-def read_sensors(relay='A', settling_time=None):
-    return reader.read_sensors(relay, settling_time)
