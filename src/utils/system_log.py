f17920b8e342ea06834a6fc4c9ab5810d09b1bdc
Eduardo Santos <3dmena@gmail.com>
2025-04-03 04:04:03 -0600

    fix: removed path attr form os since its not valid in micropython

--- src/utils/system_log.py
+++ src/utils/system_log.py
@@ -1,18 +1,32 @@
-"""System log file"""
+"""MicroPython system logger with log rotation at 300KB."""
+
 import os
 
 LOG_FILE = "data/log.txt"
-MAX_LOG_SIZE = 1024 * 300  # 300 KB limit
+MAX_LOG_SIZE = 1024 * 300  # 300 KB
 
 def print_to_log(*args):
-    """Append a log message to the system log file rotating with 300KB limit."""
+    """
+    Append a message to the system log file.
+
+    If the log file exceeds MAX_LOG_SIZE, it will be cleared before writing.
+    Accepts multiple arguments which are joined into a single line.
+    """
     msg = " ".join(map(str, args)) + "\n"
     try:
-        if os.path.exists(LOG_FILE):
-            if os.stat(LOG_FILE).st_size > MAX_LOG_SIZE:
-                with open(LOG_FILE, "w", encoding="utf-8") as f:
-                    f.write("")
+        size = 0
+        try:
+            with open(LOG_FILE, "rb") as f:
+                f.seek(0, 2)
+                size = f.tell()
+        except OSError:
+            pass  # File doesn't exist yet
+
+        if size > MAX_LOG_SIZE:
+            with open(LOG_FILE, "w") as f:
+                f.write("")
+
+        with open(LOG_FILE, "a") as f:
+            f.write(msg)
     except OSError:
         pass
-    with open(LOG_FILE, "a", encoding="utf-8") as f:
-        f.write(msg)
