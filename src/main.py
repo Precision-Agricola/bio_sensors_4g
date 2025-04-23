b8719d5667a5e7b740b443fb468907d0681a4c66
Eduardo Santos <3dmena@gmail.com>
2025-04-23 01:44:55 -0600

    feat: wdt adden into main script

--- src/main.py
+++ src/main.py
@@ -1,15 +1,26 @@
 import uasyncio as asyncio
 import config.runtime as config
 from routines.aerator_3hr import turn_on_aerators
+from machine import WDT
+
+wdt = WDT(timeout=1000 * 60 * 5)
+
+async def feed_watchdog():
+    """Task to periodically feed the watchdog"""
+    while True:
+        wdt.feed()
+        await asyncio.sleep(5)
 
 async def run_async_mode(sensor_routine, ws_client):
     from utils.retry_loop import retry_loop
+    asyncio.create_task(feed_watchdog())
     asyncio.create_task(retry_loop(sensor_routine))
     await ws_client(sensor_routine)
 
 def main():
     mode = config.get_mode()
     print(f"BIO-IOT v1.2 - Mode: {mode}")
+    wdt.feed()
 
     if mode == "PROGRAM MODE":
         print("Program mode active - Development interfaces enabled")
@@ -21,7 +32,7 @@ def main():
 
         sensor_routine = SensorRoutine()
         sensor_routine.start()
-        _thread.start_new_thread(turn_on_aerators, ())
+        _thread.start_new_thread(turn_on_aerators, (wdt,))
         
         # Ejecuta WebSocket + retry loop
         asyncio.run(run_async_mode(sensor_routine, websocket_client))
@@ -33,7 +44,7 @@ def main():
 
         sensor_routine = SensorRoutine()
         sensor_routine.start()
-        _thread.start_new_thread(turn_on_aerators, ())
+        _thread.start_new_thread(turn_on_aerators, (wdt,))
         
         # Ejecuta WebSocket + retry loop
         asyncio.run(run_async_mode(sensor_routine, websocket_client))
