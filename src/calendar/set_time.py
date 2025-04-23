2d7af78df842ece791813a5110ee3b73a2e26cf2
Eduardo Santos <3dmena@gmail.com>
2025-04-23 17:48:35 -0600

    feat:updated calendar features

--- src/calendar/set_time.py
+++ src/calendar/set_time.py
@@ -1,22 +1,25 @@
+# src/calendar/set_time.py
+
 from machine import Pin
 import calendar.ds1302 as ds1302
-from config.config import load_device_config
+try:
+    from config.config import RTC_CLK_PIN, RTC_DIO_PIN, RTC_CS_PIN
+except ImportError:
+    print("WARN: No se encontraron constantes RTC_..._PIN en config.config, usando defaults 16, 21, 23.")
+    RTC_CLK_PIN = 16
+    RTC_DIO_PIN = 21
+    RTC_CS_PIN = 23
 
-DEFAULT_RTC_CONFIG = {
-    "clk_pin": 16,
-    "dio_pin": 21,
-    "cs_pin": 23
-}
 
 def init_rtc(config=None):
-    if config is None:
-        device_config = load_device_config()
-        rtc_config = device_config.get('rtc', DEFAULT_RTC_CONFIG)
-    else:
-        rtc_config = config
-    clk = Pin(rtc_config.get('clk_pin', DEFAULT_RTC_CONFIG['clk_pin']))
-    dio = Pin(rtc_config.get('dio_pin', DEFAULT_RTC_CONFIG['dio_pin']))
-    cs = Pin(rtc_config.get('cs_pin', DEFAULT_RTC_CONFIG['cs_pin']))
+    clk_pin_num = RTC_CLK_PIN
+    dio_pin_num = RTC_DIO_PIN
+    cs_pin_num = RTC_CS_PIN
+
+    clk = Pin(clk_pin_num)
+    dio = Pin(dio_pin_num)
+    cs = Pin(cs_pin_num)
+
     return ds1302.DS1302(clk, dio, cs)
 
 def compute_weekday(day, month, year):
