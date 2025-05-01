04dead129851939a9610fd2575b3b5fd92d274d6
Eduardo Santos <3dmena@gmail.com>
2025-04-30 22:35:58 -0600

    feat: relays opt

--- src/system/control/relays.py
+++ src/system/control/relays.py
@@ -2,73 +2,56 @@ from machine import Pin
 import time
 
 class SensorRelay:
-    def __init__(self, relay_pin_a=13, relay_pin_b=14):
-        self.relay_a = Pin(relay_pin_a, Pin.OUT, value=0)
-        self.relay_b = Pin(relay_pin_b, Pin.OUT, value=0)
-        self.active_relay = None
-    
+    def __init__(self, pin_a=13, pin_b=14):
+        self.a = Pin(pin_a, Pin.OUT, value=0)
+        self.b = Pin(pin_b, Pin.OUT, value=0)
+        self.active = None
+
     def activate_a(self):
-        self.relay_b.on()
-        self.relay_a.on()
-        self.active_relay = 'A'
-    
+        self.a.on()
+        self.b.on()
+        self.active = 'A'
+
     def activate_b(self):
-        self.relay_a.on()
-        self.relay_b.on()
-        self.active_relay = 'B'
-    
+        self.a.on()
+        self.b.on()
+        self.active = 'B'
+
     def deactivate_all(self):
-        self.relay_a.off()
-        self.relay_b.off()
-        self.active_relay = None
-    
+        self.a.off()
+        self.b.off()
+        self.active = None
+
     def get_active(self):
-        return self.active_relay
+        return self.active
 
 
 class LoadRelay:
-    def __init__(self, relay_pins=(0, 27)):
-        self.relays = [Pin(pin, Pin.OUT, value=0) for pin in relay_pins]
-    
-    def turn_on(self, relay_index=None):
-        if relay_index is None:
-            # Turn on all relays
-            for relay in self.relays:
-                relay.on()
-        elif 0 <= relay_index < len(self.relays):
-            # Turn on specific relay
-            self.relays[relay_index].on()
-    
-    def turn_off(self, relay_index=None):
-        if relay_index is None:
-            # Turn off all relays
-            for relay in self.relays:
-                relay.off()
-        elif 0 <= relay_index < len(self.relays):
-            # Turn off specific relay
-            self.relays[relay_index].off()
-    
-    def cycle(self, on_time, off_time, cycles=1, watchdog=None):
-       for _ in range(cycles):
-            # Turn on phase
+    def __init__(self, pins=(0, 27)):
+        self.relays = [Pin(p, Pin.OUT, value=0) for p in pins]
+
+    def turn_on(self, idx=None):
+        targets = self.relays if idx is None else [self.relays[idx]]
+        for r in targets: r.on()
+
+    def turn_off(self, idx=None):
+        targets = self.relays if idx is None else [self.relays[idx]]
+        for r in targets: r.off()
+
+    def cycle(self, on_t, off_t, n=1, wdt=None):
+        for _ in range(n):
             self.turn_on()
-            self._wait_with_watchdog(on_time, watchdog)
-            
-            # Turn off phase
+            self._wait(on_t, wdt)
             self.turn_off()
-            self._wait_with_watchdog(off_time, watchdog)
-    
-    def _wait_with_watchdog(self, seconds, watchdog=None):
-        start = time.time()
-        while time.time() - start < seconds:
+            self._wait(off_t, wdt)
+
+    def _wait(self, secs, wdt=None):
+        t0 = time.time()
+        while time.time() - t0 < secs:
             time.sleep(1)
-            if watchdog:
-                watchdog.feed()
+            if wdt: wdt.feed()
 
-    def get_state(self, relay_index=None):
-        if relay_index is None:
-            return [relay.value() == 1 for relay in self.relays]
-        elif 0 <= relay_index < len(self.relays):
-            return self.relays[relay_index].value() == 1
-        return None
-
+    def get_state(self, idx=None):
+        if idx is None:
+            return [r.value() == 1 for r in self.relays]
+        return self.relays[idx].value() == 1 if 0 <= idx < len(self.relays) else None
