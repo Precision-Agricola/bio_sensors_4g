7dce5e9f5590380820fb25c05c277ce3f733d37f
Eduardo Santos <3dmena@gmail.com>
2025-04-30 18:49:47 -0600

    draft: placehoder for test implementation

---
+++ src/tests/system_tests.py
@@ -0,0 +1,9 @@
+def run_initial_tests():
+    print("Running Initial Tests")
+    print("Running RTC - to implement")
+    print("Running aerator - to implement")
+    print("Running sensors - to implement")
+    print("Running wifi - to implement")
+
+if __name__ == "__main__":
+    run_initial_tests()
