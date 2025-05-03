94417102ac8de3830830ed766d7f695975e901ba
Eduardo Santos <3dmena@gmail.com>
2025-05-02 23:28:00 -0600

    chore: restart cycle reduced and other

--- server/main.py
+++ server/main.py
@@ -2,18 +2,20 @@ import uasyncio as asyncio
 import time
 import machine
 from core.access_point import AccessPointManager
-from core.websocket_server import start_websocket_server, watchdog_feeder
+from core import websocket_server
 from utils.logger import log_message
 
 SSID = "PrecisionAgricola"
 PASSWORD = "ag2025pass"
 
 ENABLE_SCHEDULED_REBOOT = True
-REBOOT_INTERVAL_HOURS = 24
+REBOOT_INTERVAL_HOURS = 6
 REBOOT_INTERVAL_MS = REBOOT_INTERVAL_HOURS * 60 * 60 * 1000
+INITIAL_DELAY_S  = 15
 
 ap_manager = AccessPointManager(ssid=SSID, password=PASSWORD)
 ap_manager.setup_access_point()
+
 
 async def scheduled_reboot_task():
     """Tarea que espera REBOOT_INTERVAL_MS y luego reinicia el sistema."""
@@ -25,13 +27,23 @@ async def scheduled_reboot_task():
     machine.reset()
 
 async def main():
-    asyncio.create_task(watchdog_feeder())
+    log_message(f"--- Application Starting ---")
+    log_message(f"Waiting {INITIAL_DELAY_S} seconds before initializing Watchdog Timer...")
+    log_message(">>> You can press CTRL+C in Thonny now to stop <<<")
+    await asyncio.sleep(INITIAL_DELAY_S)
+
+    try:
+        websocket_server.wdt = machine.WDT(timeout=websocket_server.WDT_TIMEOUT_MS)
+        log_message("Watchdog Timer Initialized.")
+        asyncio.create_task(websocket_server.watchdog_feeder())
+    except Exception as e:
+        log_message(f"FATAL: Could not initialize Watchdog Timer: {e}")
 
     if ENABLE_SCHEDULED_REBOOT:
         asyncio.create_task(scheduled_reboot_task())
 
     log_message("Starting main application server...")
-    await start_websocket_server()
+    await websocket_server.start_websocket_server()
 
 try:
     asyncio.run(main())
