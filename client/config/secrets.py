9966e33d309038c384a3cecd1874d02498439ff8
Eduardo Santos <3dmena@gmail.com>
2025-05-22 22:11:42 -0600

    fix(removed -- wifi): removed wifi code from client

--- client/config/secrets.py
+++ client/config/secrets.py
@@ -6,15 +6,3 @@ def get_mac_suffix():
 
 # Device identification (HTTP)
 DEVICE_ID = f"ESP32_{get_mac_suffix()}"
-SERVER_IP = "192.168.4.1"
-SERVER_PORT = 80 
-
-# WiFi configuration
-WIFI_CONFIG = {
-    "ssid": "PrecisionAgricola",  # Access point SSID
-    "password": "ag2025pass",     # Access point password
-    "ws_server_uri": "ws://192.168.4.1/ws" # websocket server uri
-}
-
-# Broker IP (Microdot HTTP server)
-BROKER_IP = "192.168.4.1"
--- client/tests/system_tests.py
+++ client/tests/system_tests.py
@@ -3,7 +3,6 @@ def run_initial_tests():
     print("Running RTC - to implement")
     print("Running aerator - to implement")
     print("Running sensors - to implement")
-    print("Running wifi - to implement")
 
 if __name__ == "__main__":
     run_initial_tests()
