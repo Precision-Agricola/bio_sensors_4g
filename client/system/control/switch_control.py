1d828580ad73be97dc2ae1c1a8052eb90920d5dd
Eduardo Santos <3dmena@gmail.com>
2025-05-30 17:57:57 -0600

    chore(sytem-switc): pump control updated for NO switch

--- client/system/control/switch_control.py
+++ client/system/control/switch_control.py
@@ -7,30 +7,27 @@ from config.config import SWITCH_PIN, RECIRCULATION_POMP_PIN
 switch_pin = Pin(SWITCH_PIN, Pin.IN, Pin.PULL_UP)
 recirculation_pomp = Pin(RECIRCULATION_POMP_PIN, Pin.OUT)
 
-DEBOUNCE_MS = 50
-POLL_INTERVAL = 0.01  # 10 ms
-STABLE_COUNT = int(DEBOUNCE_MS / (POLL_INTERVAL * 1000))
+POLL_INTERVAL = 0.01
+DEBOUNCE_COUNT = 5
 
 async def monitor_switch():
     last_state = switch_pin.value()
-    stable_counter = 0
-    pomp_on = False
+    pressed = False
+    pump_on = False
 
     while True:
-        current = switch_pin.value()
+        state = switch_pin.value()
 
-        if current != last_state:
-            stable_counter = 0
-        else:
-            stable_counter += 1
+        if last_state == 1 and state == 0:
+            consistent = True
+            for _ in range(DEBOUNCE_COUNT):
+                await asyncio.sleep(POLL_INTERVAL)
+                if switch_pin.value() != 0:
+                    consistent = False
+                    break
+            if consistent:
+                pump_on = not pump_on
+                recirculation_pomp.value(pump_on)
 
-        if stable_counter >= STABLE_COUNT:
-            if current == 0 and not pomp_on:
-                recirculation_pomp.on()
-                pomp_on = True
-            elif current == 1 and pomp_on:
-                recirculation_pomp.off()
-                pomp_on = False
-
-        last_state = current
+        last_state = state
         await asyncio.sleep(POLL_INTERVAL)
