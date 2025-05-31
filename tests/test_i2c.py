e73d11dc4d08fc778e62d352f6db92dcaa458ffc
Eduardo Santos <3dmena@gmail.com>
2025-05-30 23:54:46 -0600

    chore(tests): relay for power on sensor relay removed

--- tests/test_i2c.py
+++ tests/test_i2c.py
@@ -1,10 +1,7 @@
 from machine import Pin, SoftI2C
 from config.config import I2C_SCL_PIN, I2C_SDA_PIN
-from system.control.relays import SensorRelay  # Asume que guardaste la clase allí
 import time
 
-relay = SensorRelay()
-relay.activate_a()  # O activate_b() según el sensor
 
 time.sleep(1)  # Espera a que el sensor se energice
 
