54c4faf62e142bd542a56c04ac12ce3127574ba0
Eduardo Santos <3dmena@gmail.com>
2025-05-31 20:36:37 -0600

    xhore(relays): aerator pines readed from config

--- client/system/control/relays.py
+++ client/system/control/relays.py
@@ -2,7 +2,7 @@
 
 from machine import Pin
 import time
-from config.config import AERATOR_PIN_A, AERATOR_PIN_B
+from config import runtime_config
 
 class SensorRelay:
     def __init__(self, pin_a=13, pin_b=14):
@@ -31,8 +31,10 @@ class SensorRelay:
 
 class LoadRelay:
     def __init__(self):
-        self.relays = [Pin(AERATOR_PIN_A, Pin.OUT, value=0),
-                       Pin(AERATOR_PIN_B, Pin.OUT, value=0)]
+        self.relays = [
+            Pin(runtime_config.AERATOR_PIN_A, Pin.OUT, value=0),
+            Pin(runtime_config.AERATOR_PIN_B, Pin.OUT, value=0)
+        ]
 
     def turn_on(self, idx=0):
         other = 1 - idx
@@ -43,7 +45,7 @@ class LoadRelay:
         targets = self.relays if idx is None else [self.relays[idx]]
         for r in targets: r.off()
 
-    def cycle(self, on_t, off_t, n=1, wdt=None):
+    def cycle(self, on_t, off_t, n=1, wdt=None): #TODO: legacy test routine, not used in current logic
         for _ in range(n):
             self.turn_on(0)
             self._wait(on_t, wdt)
