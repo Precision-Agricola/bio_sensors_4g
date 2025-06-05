454517c1291606ded5dea3964a17ea029eeca2b1
Eduardo Santos <3dmena@gmail.com>
2025-06-05 14:24:13 -0600

    chore (server): aws ford nos is async

--- server/core/aws_forwarding.py
+++ server/core/aws_forwarding.py
@@ -1,16 +1,14 @@
 # broker/core/aws_forwarding.py
 
+import uasyncio as asyncio
 import json
 from pico_lte.core import PicoLTE
 from machine import Pin
-import time
 from utils.logger import log_message
 from config.device_info import DEVICE_ID
 
-# LED for status indication
 led = Pin("LED", Pin.OUT)
 
-# Initialize PicoLTE for AWS IoT Core
 try:
     picoLTE = PicoLTE()
     aws_enabled = True
@@ -19,7 +17,7 @@ except Exception as e:
     aws_enabled = False
     log_message(f"Error initializing AWS IoT: {e}")
 
-def send_to_aws(data):
+async def send_to_aws(data):
     if not aws_enabled:
         log_message("AWS IoT Core not initialized, skipping upload")
         return False
@@ -34,7 +32,8 @@ def send_to_aws(data):
 
         payload = json.dumps(payload_json)
         retry_count = 3
-        for attempt in range(1, retry_count+1):
+
+        for attempt in range(1, retry_count + 1):
             result = picoLTE.aws.publish_message(payload)
             if result["status"] == 0:
                 log_message("Data sent successfully to AWS IoT Core")
@@ -42,11 +41,9 @@ def send_to_aws(data):
                 return True
             else:
                 log_message(f"Attempt {attempt}: Error sending data to AWS IoT Core")
-                time.sleep(1)
-        
+                await asyncio.sleep(1)
+
         return False
     except Exception as e:
         log_message(f"ERROR AWS: {e}")
-        import sys
-        sys.log_message_exception(e)
         return False
