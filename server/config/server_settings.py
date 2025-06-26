c1d73e004f204c39c4097dc15f3729fffc5c89b5
Eduardo Santos <3dmena@gmail.com>
2025-06-25 21:07:29 -0600

    feat(uart): comm pines moved to 4-5

--- server/config/server_settings.py
+++ server/config/server_settings.py
@@ -3,7 +3,7 @@ INITIAL_DELAY_S = 15
 REBOOT_HOURS = 24
 
 # Uart params
-TX_PIN = 12 
-RX_PIN = 13 
-UART_CHANNEL = 0
+TX_PIN = 4
+RX_PIN = 5
+UART_CHANNEL = 1 # channel 0 cant be used (cualcom - model conflict)
 UART_BAUDRATE = 9600
--- server/utils/uart.py
+++ server/utils/uart.py
@@ -1,4 +1,4 @@
-#server/utils.uart.py
+#server/utils/uart.py
 
 from machine import UART, Pin
 from config.server_settings import TX_PIN, RX_PIN, UART_CHANNEL, UART_BAUDRATE
